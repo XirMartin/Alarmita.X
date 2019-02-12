91d26a74fdbceac7a35b818ec7efdbb72e9a49bc
Martin Juarez <xir.martin@gmail.com>
2019-02-12 17:51:06 -0300

    primer prototipo

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "mcc_generated_files/mcc.h"
+
+
+#define     Porton      IO_RC0_PORT
+#define     Puerta      IO_RC1_PORT
+#define     Sirena      IO_RB7_LAT
+
+
+void main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+
+    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+
+    while (1)
+    {
+        if((Puerta == 1)&&(Porton == 1))
+        {
+            Sirena = 1;
+            __delay_ms(3000);
+            Sirena = 0;
+        }
+            
+        
+        
+        // Add your application code
+    }
+}
